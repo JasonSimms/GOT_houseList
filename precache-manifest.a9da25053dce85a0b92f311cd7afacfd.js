98e8fa4b61d721e68f98993449c3fdc0a126ac15
gitgut0110 <jsimms0110@gmail.com>
2018-11-02 10:53:05 +0100

    Updates

---
+++ precache-manifest.a9da25053dce85a0b92f311cd7afacfd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "bbb37449ab846032109d",
+    "url": "/static/js/main.bbb37449.chunk.js"
+  },
+  {
+    "revision": "dd22a1c04d146e2699fc",
+    "url": "/static/js/1.dd22a1c0.chunk.js"
+  },
+  {
+    "revision": "bbb37449ab846032109d",
+    "url": "/static/css/main.124fcea6.chunk.css"
+  },
+  {
+    "revision": "dd22a1c04d146e2699fc",
+    "url": "/static/css/1.7da93953.chunk.css"
+  },
+  {
+    "revision": "28d7be6c81b6827617d279a60c65970b",
+    "url": "/index.html"
+  }
+];
